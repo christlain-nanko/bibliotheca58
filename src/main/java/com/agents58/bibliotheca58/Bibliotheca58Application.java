67ed287c6405da0be38e5dcb79ed28aef637c545
Christlain Nanko <chrisnanko@christlainsmbp2.speedport.ip>
2025-01-09 22:43:25 +0100

    project setup

---
+++ src/main/java/com/agents58/bibliotheca58/Bibliotheca58Application.java
@@ -0,0 +1,13 @@
+package com.agents58.bibliotheca58;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bibliotheca58Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bibliotheca58Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/agents58/bibliotheca58/Bibliotheca58ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agents58.bibliotheca58;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bibliotheca58ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
